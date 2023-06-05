da8b38207afc1dbbb4fd6b773cb6da5993301a9d
mrtnvgr <martynovegorOF@yandex.ru>
2023-06-05 11:42:45 +0700

    feat(2022/01): solve

---
+++ 2022/01/src/main.rs
@@ -0,0 +1,21 @@
+use std::fs;
+
+fn main() {
+    let list = fs::read_to_string("input.txt").unwrap();
+
+    let mut elves: Vec<i32> = vec![0];
+    let mut index = 0;
+
+    for line in list.lines().map(str::trim) {
+        if line.is_empty() {
+            index += 1;
+            elves.push(0);
+            continue;
+        }
+
+        let new_callories: i32 = line.parse().unwrap();
+        elves[index] += new_callories;
+    }
+
+    println!("{}", elves.iter().max().unwrap());
+}
