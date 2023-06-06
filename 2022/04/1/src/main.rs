29e49bc1a334a4e46c261b114b1831558abcf257
mrtnvgr <martynovegorOF@yandex.ru>
2023-06-06 13:55:22 +0700

    feat(2022/04/1): solve

---
+++ 2022/04/1/src/main.rs
@@ -0,0 +1,30 @@
+use std::{fs, ops::RangeInclusive};
+
+fn main() {
+    let list = fs::read_to_string("input.txt").unwrap();
+
+    let mut counter = 0;
+
+    for line in list.lines().map(str::trim) {
+        let mut pairs = Vec::new();
+
+        for pair in line.split(',') {
+            let (start, end) = pair.split_once('-').unwrap();
+            let start = start.trim().parse::<u32>().unwrap();
+            let end = end.trim().parse::<u32>().unwrap();
+
+            pairs.push(RangeInclusive::new(start, end));
+        }
+
+        let is_overlap = |x: &RangeInclusive<u32>, y: &RangeInclusive<u32>| {
+            y.contains(x.start()) && y.contains(x.end())
+        };
+
+        if is_overlap(&pairs[0], &pairs[1]) || is_overlap(&pairs[1], &pairs[0]) {
+            println!("Contains! {pairs:?}");
+            counter += 1;
+        }
+    }
+
+    println!("{counter}");
+}
